9b720f835c1714896696e1fdf195eb2297d70e81
Bruno Felicio <bruno@brunofelicio.com>
2017-02-15 12:20:59 +0100

    removed google fonts link

--- header.php
+++ header.php
@@ -6,12 +6,10 @@
     <title><?php wp_title('|', true, 'right'); ?><?php bloginfo ('name');?></title>
     <meta name="description" content="<?php bloginfo ('description');?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="<?php echo THEMEROOT; ?>/css/normalize.css">
-    <link rel="stylesheet" href="<?php echo THEMEROOT; ?>/css/style.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.4.min.js"><\/script>')</script>
     <script src="<?php echo THEME_JS; ?>/vendor/modernizr-2.6.2.min.js"></script>
-
+    <link rel="stylesheet" href="<?php echo THEMEROOT; ?>/css/normalize.css">
+    <link rel="stylesheet" href="<?php echo THEMEROOT; ?>/css/style.css">
     <?php wp_head(); ?>
 
 </head>
