bc60eb6b36094be91222aa966870d4aac974be86
Bruno Felicio <bruno@brunofelicio.com>
2017-02-23 11:04:38 +0100

    changes to fancy box css

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$("a.screenshot").fancybox({
+		'transitionIn'		: 'fade',
+		'transitionOut'		: 'fade',
+		'showCloseButton'	: false,
+		'titlePosition' 	: 'inside',
+		'overlayOpacity'	: '0.8',
+		'overlayColor'		: '#fff'
+	});
+});
