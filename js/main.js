303d485ffadaf19dc89965e9a6b4fe1b1726d1b8
Bruno Felicio <gigafusion@gmail.com>
2018-09-15 12:18:14 +0200

    removed logs

--- js/main.js
+++ js/main.js
@@ -8,33 +8,21 @@ $(document).ready(function() {
 		$(".menu-overlay ul li").toggleClass('menu-anim').promise().done(function(){
 			$(".menu-overlay").toggleClass('overlay-anim');
 		});
-
-		/*$( ".menu-overlay" ).fadeOut( 200, function() {
-	    $(".menu-overlay").css("display", "none");
-	  });*/
 	});
 
 	$( ".mobile-menu-toggle" ).on( "click", function() {
-	  //console.log('Success!');
 
 		$(".menu-overlay").toggleClass('overlay-anim').promise().done(function(){
 			$(".menu-overlay ul li").addClass('menu-anim');
 		});
-		/*$( ".menu-overlay" ).fadeIn( 200, function() {
-	    $(".menu-overlay").css("display", "block");
-	  });*/
 
 	});
 
-
 	// End of mobile menu
-
 
 	$(".lazy").Lazy({
         beforeLoad: function(element) {
-
 					var image_height = (element).height();
-					console.log("Image height is:" + image_height);
 					$(".top-image").animate({
 						height: (image_height + 120),
 					});
@@ -48,7 +36,7 @@ $(document).ready(function() {
 
         },
         onError: function(element) {
-            // called whenever an element could not be handled
+            
         },
         onFinishedAll: function() {
 
