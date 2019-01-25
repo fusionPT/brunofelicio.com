f092a4f4229d32bbf60612f1e07f0faa82972473
Bruno Felicio <gigafusion@gmail.com>
2019-01-25 10:03:18 +0100

    add svg support for uploads

--- functions.php
+++ functions.php
@@ -28,3 +28,14 @@ function my_function_admin_bar() {
 add_filter('show_admin_bar', '__return_false');
 add_theme_support( 'post-thumbnails' );
 set_post_thumbnail_size( 300, 300, true );
+
+//add SVG to allowed file uploads
+function add_file_types_to_uploads($file_types){
+
+    $new_filetypes = array();
+    $new_filetypes['svg'] = 'image/svg+xml';
+    $file_types = array_merge($file_types, $new_filetypes );
+
+    return $file_types;
+}
+add_action('upload_mimes', 'add_file_types_to_uploads');
